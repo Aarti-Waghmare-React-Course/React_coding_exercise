8c860e57fb2f4d764dcc68de06bdcae58d87e992
Aarti Waghmare <aarticeo5@gmail.com>
2025-05-15 23:04:48 +0530

    Toggle functionality added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import CounterApp_01 from './components/CounterApp_01';
 import GreetingComponent_02 from './components/GreetingComponent_02';
+import Toggle_03 from './components/Toggle_03';
 
 function App() {
   return (
     <div>
       {/* <CounterApp_01/> */}
-      <GreetingComponent_02  name="John Luther"/>
+      {/* <GreetingComponent_02  name="John Luther"/> */}
+      <Toggle_03/>
     </div>
   );
 }
