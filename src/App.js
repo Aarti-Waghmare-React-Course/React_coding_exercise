99dae62a3dd6d7260a5c80b2143205e1185f10ef
Aarti Waghmare <aarticeo5@gmail.com>
2025-05-15 22:45:17 +0530

    Greeting hello added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterApp_01 from './components/CounterApp_01';
+import GreetingComponent_02 from './components/GreetingComponent_02';
 
 function App() {
   return (
     <div>
-      <CounterApp_01/>
+      {/* <CounterApp_01/> */}
+      <GreetingComponent_02  name="John Luther"/>
     </div>
   );
 }
---
+++ src/components/GreetingComponent_02.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+const GreetingComponent_02 = (props) =>{
+
+    return(
+        <div>
+         <h2>Hello,{props.name}</h2>
+        </div>
+    );
+};
+
+export default GreetingComponent_02;
