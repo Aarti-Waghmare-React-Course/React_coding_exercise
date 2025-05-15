498873934f50319767825820741d41fa4078970f
Aarti Waghmare <aarticeo5@gmail.com>
2025-05-15 22:36:44 +0530

    CounterApp_01 running successfully

---
+++ src/components/CounterApp_01.js
@@ -0,0 +1,30 @@
+import react from 'react';
+import {useState} from 'react';
+
+const CounterApp_01=() =>{
+
+    //initial state and function to change the state
+    const [count,setCount]=useState(0);
+
+    //function for increment
+    const handleIncrement = ()=>{
+        setCount(count+1);
+    };
+
+    //function for decrement
+    const handleDecrement = () =>{
+        setCount(count-1);
+    };
+
+
+    return(
+        <div>
+            <button onClick={handleIncrement}>Increment +</button>
+            <button>{count}</button>
+            <button onClick={handleDecrement}>Decrement -</button>
+        </div>
+
+    );
+};
+
+export default CounterApp_01;
